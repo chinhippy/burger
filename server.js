0ded686ecf7dcfb1a7490ab8dbdc44d89f8fa857
Anton Kogan <34128946+chinhippy@users.noreply.github.com>
2019-01-23 16:16:55 -0600

    node packages initialized and server.js core complete

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const PORT = process.env.PORT || 8080;
+const app = express();
+const routes = require('./controller/burgers_controller');
+  
+// Set "Public" as static so can use CSS/jQuery with Handlebar files
+app.use(express.static('public'));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Sets Handlebars to serve HTML
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Gives server access to routes created
+app.use(routes);
+
+// Starts the server so it can listen for requests
+app.listen(PORT, function() {
+  console.log('Server listening on PORT ' + PORT)
+})
